61335631f7bb0ad65bb8d90f3dc244426b252f85
ashubawork <andrew.shuba2@gmail.com>
2023-12-06 14:22:27 +0200

    - disable default email to a new user

--- includes/core/class-mail.php
+++ includes/core/class-mail.php
@@ -18,6 +18,7 @@ class Mail {
 	 */
 	public function __construct() {
 		add_action( 'jb_job_submission_after_create_account', array( &$this, 'send_email' ), 10, 1 );
+		add_filter( 'jb_job_email_notify', array( &$this, 'jb_job_email_notify' ), 10, 1 );
 	}
 
 
@@ -29,10 +30,28 @@ class Mail {
 	 * @since 1.0.8
 	 */
 	public function send_email( $user_id ) {
-		if ( JB()->options()->get( 'account-required' ) && UM()->options()->get( 'welcome_email_jobboardwp' ) ) {
+		if ( UM()->options()->get( 'welcome_email_jobboardwp' ) ) {
 			$user  = get_userdata( $user_id );
 			$email = $user->user_email;
 			UM()->mail()->send( $email, 'welcome_email' );
 		}
+	}
+
+
+	/**
+	 * Disable default JobBoard email notification for registered users
+	 *
+	 * @param string $notify
+	 *
+	 * @return string
+	 *
+	 * @since 1.0.8
+	 */
+	public function jb_job_email_notify( $notify ) {
+		if ( UM()->options()->get( 'welcome_email_jobboardwp' ) ) {
+			$notify = 'admin';
+		}
+
+		return $notify;
 	}
 }
